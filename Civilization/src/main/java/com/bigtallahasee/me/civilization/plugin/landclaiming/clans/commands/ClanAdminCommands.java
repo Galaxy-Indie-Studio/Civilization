50c268d2bae959fb06240c38e53b5467d55a5761
BigTallahasee <85016240+BigTallahasee@users.noreply.github.com>
2021-11-09 20:19:23 -0600

    Add files via upload

---
+++ Civilization/src/main/java/com/bigtallahasee/me/civilization/plugin/landclaiming/clans/commands/ClanAdminCommands.java
@@ -0,0 +1,17 @@
+package com.bigtallahasee.me.civilization.plugin.landclaiming.clans.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ClanAdminCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+
+
+
+        return false;
+    }
+}
