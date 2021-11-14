e7b980302f6907248b596ef005e73d0c599e557e
BigTallahasee <85016240+BigTallahasee@users.noreply.github.com>
2021-11-13 23:06:31 -0600

    Add files via upload

---
+++ Civilization v1.3/src/main/java/com/bigtallahasee/me/civilization/utils/Settings.java
@@ -0,0 +1,7 @@
+package com.bigtallahasee.me.civilization.utils;
+
+public class Settings {
+
+    public static String PLUGIN_URL = "https://spigotmc.org/resources/";
+    public static String VERSION = "1.2";
+}
